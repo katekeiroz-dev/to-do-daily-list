81ac9f09c1fb945a89d8af76e26d306a53162f55
Kate Keiroz <81806904+katekeiroz-dev@users.noreply.github.com>
2025-01-14 13:13:59 +0000

    first-commit

---
+++ script.js
@@ -0,0 +1,34 @@
+var close = document.getElementsByClassName("close");
+
+function addTask() {
+    var li = document.createElement("li");
+    var inputValue = document.getElementById("inputTask").value;
+    var t = document.createTextNode(inputValue);
+    li.appendChild(t);
+    if (inputValue === '') {
+        alert("You must write something!");
+    } else {
+        document.getElementById("taskList").appendChild(li);
+    }
+    document.getElementById("inputTask").value = "";
+
+    var span = document.createElement("SPAN");
+    var txt = document.createTextNode("\u00D7");
+    span.className = "close";
+    span.appendChild(txt);
+    li.appendChild(span);
+
+    for (let i = 0; i < close.length; i++) {
+        close[i].onclick = function () {
+            var div = this.parentElement;
+            div.style.display = "none";
+        }
+    }
+}
+
+var list = document.querySelector('ul');
+list.addEventListener('click', function (ev) {
+    if (ev.target.tagName === 'LI') {
+        ev.target.classList.toggle('checked');
+    }
+}, false);
